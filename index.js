751d705156a0acb7faebd47ab8f70190f43ee606
刘浩 <mr_hao_97@163.com>
2022-10-19 09:37:05 +0800

    修改标题
    修改企业通知标题

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const { autoGame } = require('./src/games/autoRun');
 
   try {
     const html = `
-      <h1 style="text-align: center">LuckHao自动签到通知</h1>
+      <h1 style="text-align: center">lh的自动签到通知</h1>
       <p style="text-indent: 2em">沾喜气结果：${dip_res}</p>
       <p style="text-indent: 2em">当前矿石：${now_score}</p>
       <p style="text-indent: 2em">较昨日增长：${now_score - yesterday_score}</p>
@@ -75,7 +75,7 @@ const { autoGame } = require('./src/games/autoRun');
   }
 
   try {
-    const msg = `自动签到通知:
+    const msg = `lh的自动签到通知:
       沾喜气结果：${dip_res}
       当前矿石：${now_score}
       较昨日增长：${now_score - yesterday_score}
@@ -92,7 +92,7 @@ const { autoGame } = require('./src/games/autoRun');
   }
 
   try {
-    const html = `掘金自动签到通知\n> 沾喜气结果:<font color=\"comment\">${dip_res}</font>\n> 当前矿石:<font color=\"comment\">${now_score}</font>\n> 较昨日增长:<font color=\"comment\">${now_score - yesterday_score}</font>\n>签到结果:<font color=\"comment\">${sign_res}</font>\n> 抽奖结果:<font color=\"comment\">${draw_res}</font>\n> 游戏结果:<font color=\"comment\">${game_res}</font>`
+    const html = `lh的掘金自动签到通知\n> 沾喜气结果:<font color=\"comment\">${dip_res}</font>\n> 当前矿石:<font color=\"comment\">${now_score}</font>\n> 较昨日增长:<font color=\"comment\">${now_score - yesterday_score}</font>\n>签到结果:<font color=\"comment\">${sign_res}</font>\n> 抽奖结果:<font color=\"comment\">${draw_res}</font>\n> 游戏结果:<font color=\"comment\">${game_res}</font>`
 
     const msg = await sendWxWork(html)
 
